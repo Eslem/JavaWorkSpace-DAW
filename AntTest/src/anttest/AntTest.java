49e405553e2d9d7308761c9ba0754a932405326d
Eslem Alzate <j.eslem03@gmail.com>
2014-10-02 23:17:31 +0200

    upload desktop

---
+++ AntTest/src/anttest/AntTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anttest;
+
+/**
+ *
+ * @author eslem
+ */
+public class AntTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyMath myMath = new MyMath();
+        System.out.println("5x20="+myMath.multi(5, 20));
+    }
+    
+}
